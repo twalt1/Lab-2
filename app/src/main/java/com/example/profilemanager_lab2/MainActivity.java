fbbe52d3adb0ee8acc051fe98fdf4828cc2d7a81
tuna-b <abolu089@uottawa.ca>
2021-10-06 15:44:34 -0400

    Added logos to drawables file, created onClick method for google maps button

--- app/src/main/java/com/example/profilemanager_lab2/MainActivity.java
+++ app/src/main/java/com/example/profilemanager_lab2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.profilemanager_lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onOpenGoogleMaps(View view) {
+
+        //Takes input from postalCode id and converts it to EditText, stores into teamAddress
+        EditText teamAddress = (EditText) findViewById(R.id.postalCode);
+
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q=" + teamAddress.getText());
+
+        //Create a URI from a string. Use result to create an intent
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+        //make intent explicit by setting Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        //Attempt to start the activity that can handle the intent
+        startActivity(mapIntent);
     }
 }
